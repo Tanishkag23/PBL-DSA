2e96a92d5edab655617a134560dd5cb7c0ee210a
ankitsaini0506 <ankitsaini00000506@gmail.com>
2025-10-08 21:15:56 +0530

    Delete auth.h

--- auth.h
+++
@@ -1,9 +0,0 @@
-// auth.h
-#ifndef AUTH_H
-#define AUTH_H
-
-/* handles signup/login; returns logged-in username string (caller must not free)
-   returns NULL on failure/exit */
-char* auth_prompt(); 
-
-#endif
